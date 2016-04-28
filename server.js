4f61d7f90902235ff5292059c088adb203df50bf
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-04-28 09:02:44 -0700

    3. add home page template

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
+var exphbs = require('express-handlebars');
 var port = process.env.PORT || 3000;
+
+// =======================
+// middleware & config
+// =======================
+app.set('views', 'views');
+
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: './views/layouts'
+}));
+
+app.set('view engine', 'hbs');
 
 // =======================
 // routes
